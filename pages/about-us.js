a0cfd6be6e4353beaf9a2b62fe002c4a8d4f8609
RichardNk24 <richardsobelnkulu@gmail.com>
2023-01-06 12:04:51 +0200

    Titles & Paragraphs in Index and About us

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const aboutUs = () => {
   return (
-    <h1>About Us</h1>  
+    <div>
+      <h1>About Us</h1>
+      <p>Nam posuere consequat nulla, laoreet posuere urna eleifend non. 
+        Fusce neque massa, condimentum eget nibh in, fermentum accumsan massa. 
+        Aenean eu lectus sed risus sodales faucibus non pulvinar elit. 
+        Nunc eget consectetur dolor. Sed aliquet tortor massa, ac dignissim augue tincidunt eu. 
+        Quisque cursus finibus neque quis pretium. Praesent at lectus vestibulum, interdum diam volutpat, cursus neque. 
+        Proin at arcu ac nulla egestas sodales.
+      </p>
+    </div> 
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,38 @@ export default function Home() {
         </nav>
       </header>
       <main className={styles.main}>
-        <h1 className={styles.title}></h1>
+        <div>
+          <img />
+          <h2>Event in Dakar</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          </p>
+        </div>
+        <div>
+          <img />
+          <h2>Event in Nairobi</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          </p>
+          <div>
+          <img />
+          <h2>Event in Kinshasa</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          </p>
+          <div>
+          <img />
+          <h2>Event in Lagos</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          </p>
+        </div>
+        </div>
+        </div>
       </main>
       <footer className={styles.footer}></footer>
     </>
