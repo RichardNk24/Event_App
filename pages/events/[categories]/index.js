0e634887f36ae50cac399b7befba16a2b74455f0
RichardNk24 <richardsobelnkulu@gmail.com>
2023-02-04 12:24:07 +0200

    Added 7th event

--- pages/events/[categories]/index.js
+++ pages/events/[categories]/index.js
@@ -8,7 +8,7 @@ const SingleEvent = () => {
             <a href="/events/event4">event 4</a>
             <a href="/events/event5">event 5</a>
             <a href="/events/event3">event 6</a>
-            <a href="/events/event4">event 4</a>
+            <a href="/events/event4">event 7</a>
             <a href="/events/event5">event 5</a>
         </div>
         )
